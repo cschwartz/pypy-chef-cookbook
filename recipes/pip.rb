846bc989d5afbf067a8156896622d3cc2e324383
christian.schwartz@gmail.com <christian.schwartz@gmail.com>
2013-07-19 15:48:44 +0200

    added pip

---
+++ recipes/pip.rb
@@ -0,0 +1,44 @@
+# author:: Seth Chisamore <schisamo@opscode.com>
+# Cookbook Name:: python
+# Recipe:: pip
+#
+# Copyright 2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Where does pip get installed?
+# platform/method: path (proof)
+# redhat/package: /usr/bin/pip (sha a8a3a3)
+# omnibus/source: /opt/local/bin/pip (sha 29ce9874)
+
+pip_binary = "/usr/bin/pip"
+
+# Ubuntu's python-setuptools, python-pip and python-virtualenv packages
+# are broken...this feels like Rubygems!
+# http://stackoverflow.com/questions/4324558/whats-the-proper-way-to-install-pip-virtualenv-and-distribute-for-python
+# https://bitbucket.org/ianb/pip/issue/104/pip-uninstall-on-ubuntu-linux
+remote_file "#{Chef::Config[:file_cache_path]}/distribute_setup.py" do
+  source node['pypy']['distribute_script_url']
+  mode "0644"
+  not_if { ::File.exists?(pip_binary) }
+end
+
+execute "install-pip" do
+  cwd Chef::Config[:file_cache_path]
+  command <<-EOF
+  #{node.pypy.deb.installation_dir}/bin/pypy distribute_setup.py --download-base=#{node['pypy']['distribute_option']['download_base']}
+  #{::File.dirname(pip_binary)}/easy_install pip
+  EOF
+  not_if { ::File.exists?(pip_binary) }
+end
